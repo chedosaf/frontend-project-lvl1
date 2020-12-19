f18af67044be8afddf8e205f0f18f39a39f5ee1c
Sasha Chadaev <chedosaf@mail.ru>
2020-12-19 18:55:13 +0300

    edit linter calc.js

--- games/calc.js
+++ games/calc.js
@@ -1,22 +1,25 @@
 import readlineSync from 'readline-sync';
-import { answersToWin, maxRandomNumder, askName, userNameThis, winLoose, acc } from '../src/index.js';;
 
-export const calc = () => {
-    askName();
-    console.log('What is the result of the expression?');
-    const c = () => {
-        if (acc > answersToWin - 1) {
-            console.log(`Congratulations, ${userNameThis}!`);
-            return;
-        };
-        const operatinsMass = ['-', '+', '*'];
-        const number1 = Math.floor(Math.random() * (Math.floor(maxRandomNumder) - Math.ceil(0))) + Math.ceil(0);
-        const number2 = Math.floor(Math.random() * (Math.floor(maxRandomNumder) - Math.ceil(0))) + Math.ceil(0);
-        const operation = operatinsMass[Math.floor(Math.random() * (Math.floor(3) - Math.ceil(0))) + Math.ceil(0)];
-        console.log(`Question: ${number1} ${operation} ${number2}`);
-        let answer = readlineSync.question('Your answer: ');
-        const trueAnswer = eval(`${number1} ${operation} ${number2}`);
-        winLoose(answer, trueAnswer, c);
+import {
+  answersToWin, maxRandomNumder, askName, userNameThis, winLoose, acc,
+} from '../src/index.js';
+
+export default const calc = () => {
+  askName();
+  console.log('What is the result of the expression?');
+  const c = () => {
+    if (acc > answersToWin - 1) {
+      console.log(`Congratulations, ${userNameThis}!`);
+      return;
     };
-    c();
+    const operatinsMass = ['-', '+', '*'];
+    const number1 = Math.floor(Math.random() * (Math.floor(maxRandomNumder) - Math.ceil(0))) + Math.ceil(0);
+    const number2 = Math.floor(Math.random() * (Math.floor(maxRandomNumder) - Math.ceil(0))) + Math.ceil(0);
+    const operation = operatinsMass[Math.floor(Math.random() * (Math.floor(3) - Math.ceil(0))) + Math.ceil(0)];
+    console.log(`Question: ${number1} ${operation} ${number2}`);
+    let answer = readlineSync.question('Your answer: ');
+    const trueAnswer = eval(`${number1} ${operation} ${number2}`);
+    winLoose(answer, trueAnswer, c);
+  };
+  c();
 };
