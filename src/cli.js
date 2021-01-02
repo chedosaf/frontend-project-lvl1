b23b21a88fb0b3ba2e4c93229e02cf53be509d1c
Sasha Chadaev <chedosaf@mail.ru>
2021-01-02 22:15:32 +0300

    update progression

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 const askName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 export default askName;
--- src/games/progression.js
+++ src/games/progression.js
@@ -8,7 +8,7 @@ const progression = () => {
   const gameDescription = 'What number is missing in the progression?';
   const getRound = () => {
     const mass = [];
-    const massLength = generateRandomNumber(5, 11);
+    const massLength = 5;
     let randomNumber = generateRandomNumber(0, maxRandomNumder);
     const randomIncrease = generateRandomNumber(0, 30);
     const randomMassItem = generateRandomNumber(0, massLength);
