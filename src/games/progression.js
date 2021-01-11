387c601c1ea9fa6a8a676f2da86ec5c37b2eaabf
Sasha Chadaev <chedosaf@mail.ru>
2021-01-11 10:20:50 +0300

    edit becouse of mentour comments pt.5.3

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,7 +9,7 @@ const gameDescription = 'What number is missing in the progression?';
 const generateProgression = (length, incr, firstMember) => {
   const progression = [];
   let progressionMember = firstMember;
-  for (let i = 0; progression.length < length; i += 1) {
+  while (progression.length < length) {
     progressionMember += incr;
     progression.push(progressionMember);
   }
@@ -17,7 +17,7 @@ const generateProgression = (length, incr, firstMember) => {
 };
 
 const getRound = () => {
-  const increase = generateRandomNumber(0, 30);
+  const increase = generateRandomNumber(1, 30);
   const progressionLength = generateRandomNumber(6, 11);
   const firstProgressionMember = generateRandomNumber(0, maxRandomNumder);
   const progressionItemToHide = generateRandomNumber(0, progressionLength);
